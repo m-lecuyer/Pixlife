6d43a6988d905734f4cc2db53ca1cb45b523c4ef
Mathias <mathias@lecuyer.me>
2012-09-01 21:21:11 +0200

    Initial Commit

---
+++ LS/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  LS
+//
+//  Created by Mathias Lécuyer on 01/09/12.
+//  Copyright Polytechnique 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ LS/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  LS
+//
+//  Created by Mathias Lécuyer on 01/09/12.
+//  Copyright Polytechnique 2012. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
